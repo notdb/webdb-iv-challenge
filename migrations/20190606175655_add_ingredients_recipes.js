65d6bef3f50ec4370afec565b0965db14d347887
Doug <me@douglasbowen.com>
2019-06-06 18:01:12 -0400

    add ingredients_recipes table

---
+++ migrations/20190606175655_add_ingredients_recipes.js
@@ -0,0 +1,21 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("ingredients_recipes", function(table) {
+    table.increments();
+
+    table
+      .integer("recipe_id")
+      .notNullable()
+      .references("id")
+      .inTable("recipes");
+    table
+      .integer("ingredient_id")
+      .notNullable()
+      .references("id")
+      .inTable("ingredients");
+    table.integer("ingredient_amount").notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("ingredients_recipes");
+};
